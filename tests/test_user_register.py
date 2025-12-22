8034956d48c0bf0ed0a83a28d39da32fe317de9b
Yapteva Marina <MYapteva@croc.ru>
2025-12-23 02:01:22 +0300

    Ex15

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,9 +1,21 @@
+import random
+import string
+
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
+import pytest
 
 
 class TestUserRegister(BaseCase):
+    params = [
+        "email",
+        "password",
+        "username",
+        "firstName",
+        "lastName"
+    ]
+
     def test_create_user_sucessfully(self):
         data = self.prepare_registration_data()
 
@@ -21,5 +33,41 @@ class TestUserRegister(BaseCase):
         Assertions.assert_status_code(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
             f"Unexpected response.content: {response.content}"
+
+    def test_create_user_with_invalid_email(self):
+        data = self.prepare_registration_data("invalide_email.com")
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.text == "Invalid email format"
+
+    @pytest.mark.parametrize("missing_param", params)
+    def test_create_user_without_required_param(self, missing_param):
+        data = self.prepare_registration_data()
+        data.pop(missing_param)
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.text == f"The following required params are missed: {missing_param}"
+
+    def test_create_user_with_short_username(self):
+        data = self.prepare_registration_data()
+        data["username"] = "a"
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.text == "The value of 'username' field is too short"
+
+    def test_create_user_with_long_username(self):
+        data = self.prepare_registration_data()
+        data["username"] = ''.join(random.choices(string.ascii_letters + string.digits, k=251))
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.text == "The value of 'username' field is too long"
 
 
