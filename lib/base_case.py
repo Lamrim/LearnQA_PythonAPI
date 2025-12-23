231e1a2e9faba9d9f3359adb0e15dc91f09abc9a
Yapteva Marina <MYapteva@croc.ru>
2025-12-23 03:01:26 +0300

    Ex17

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,6 +1,8 @@
 import json
 from datetime import datetime
 from requests import Response
+from lib.my_requests import MyRequests
+
 
 class BaseCase:
     def get_cookie(self, response: Response, cookie_name):
@@ -34,3 +36,18 @@ class BaseCase:
             "email": email,
             "password": "123"
         }
+
+    def create_and_login_user(self, custom_data=None):
+        #REGISTER
+        register_data = custom_data or self.prepare_registration_data()
+        response = MyRequests.post("/user/", data=register_data)
+        assert response.status_code == 200, f"User creation failed: {response.text}"
+        user_id = self.get_json_value(response, "id")
+
+        #LOGIN
+        login_data = {"email": register_data["email"], "password": register_data["password"]}
+        login_response = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(login_response, "auth_sid")
+        token = self.get_header(login_response, "x-csrf-token")
+
+        return user_id, auth_sid, token
--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -4,42 +4,56 @@ from lib.my_requests import MyRequests
 
 
 class TestUserEdit(BaseCase):
-    def test_edit_just_created_uer(self):
-        #REGISTER
-        register_data = self.prepare_registration_data()
-        response1 = MyRequests.post("/user/", data=register_data)
 
-        Assertions.assert_status_code(response1, 200)
-        Assertions.assert_json_has_key(response1, "id")
+    def edit_user(self, user_id, auth_sid=None, token=None, data=None):
+        headers = {"x-csrf-token": token}
+        cookies = {"auth_sid": auth_sid}
 
-        email = register_data["email"]
-        first_name = register_data["firstName"]
-        password = register_data["password"]
-        user_id = self.get_json_value(response1, "id")
+        return MyRequests.put(f"/user/{user_id}", headers=headers, cookies=cookies, data=data)
 
-        #LOGIN
-        login_data = {
-            "email": email,
-            "password": password
-        }
+    def test_edit_just_created_user(self):
+        user_id, auth_sid, token = self.create_and_login_user()
 
-        response2 = MyRequests.post("/user/login", data=login_data)
-
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
-
-        #EDIT
         new_name = "New name"
-        response3 = MyRequests.put(f"/user/{user_id}",
-                                 headers={"x-csrf-token": token},
-                                 cookies={"auth_sid": auth_sid},
-                                 data={"firstName": new_name})
+        response = self.edit_user(user_id, auth_sid, token, {"firstName": new_name})
 
-        Assertions.assert_status_code(response3, 200)
+        Assertions.assert_status_code(response, 200)
 
-        #GET
-        response4 = MyRequests.get(f"/user/{user_id}",
-                                 headers={"x-csrf-token": token},
-                                 cookies={"auth_sid": auth_sid})
+        response_get = MyRequests.get(f"/user/{user_id}",
+                                      headers={"x-csrf-token": token},
+                                      cookies={"auth_sid": auth_sid}
+        )
 
-        Assertions.assert_json_value_by_name(response4, "firstName", new_name, "Wrong name")
+        Assertions.assert_json_value_by_name(response_get, "firstName", new_name, "Wrong name")
+
+    def test_edit_user_without_authorization(self):
+        user_id, auth_sid, token = self.create_and_login_user()
+        new_name = "New name"
+
+        response = self.edit_user(user_id, data={"firstName": new_name})
+
+        Assertions.assert_status_code(response, 400)
+
+    def test_edit_as_other_user(self):
+        user_id, auth_sid, token = self.create_and_login_user()
+
+        other_user_id = 2
+        new_name = "New name"
+
+        response = self.edit_user(other_user_id, auth_sid, token, {"firstName": new_name})
+
+        Assertions.assert_status_code(response, 400)
+
+    def test_edit_with_invalid_email(self):
+        user_id, auth_sid, token = self.create_and_login_user()
+        invalid_email = "invalidemail.com"
+        response = self.edit_user(user_id, auth_sid, token, {"email": invalid_email})
+
+        Assertions.assert_status_code(response, 400)
+
+    def test_edit_with_short_name(self):
+        user_id, auth_sid, token = self.create_and_login_user()
+        short_name = "M"
+        response = self.edit_user(user_id, auth_sid, token, {"firstName": short_name})
+
+        Assertions.assert_status_code(response, 400)
